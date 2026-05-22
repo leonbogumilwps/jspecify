335221e893026700b290684b9c75065bf878f7af
lbogumil <leon.bogumil@wps.de>
2026-05-22 15:52:36 +0200

    init

---
+++ src/test/java/de/lbo/jspecify_demo/JspecifyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lbo.jspecify_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspecifyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
